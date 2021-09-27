8d65dca23a1212865ff169f9fd8f04b38dac50b4
원치현 <heungeob1003@gmail.com>
2021-09-27 19:39:35 +0900

    Create 2 dimentional Array

--- reduceMethod.js
+++ reduceMethod.js
@@ -6,3 +6,22 @@ a.reduce(function(prev,value){
 a.reduce(function(prev,value){
     return prev + value;
 }); // -> 15 3->6->10->15 
+
+var a = [1,2,3,4,5];
+a.reduce(function(p,v){
+    return p*v;
+},1); //->120 : 배열 요소의 곱
+a.reduce(function(p,v){
+    return (p>v)?p:v;
+}); // ->5 : 최댓값
+
+var a = ["Tom","Huck","Becky"];
+a.reduce(function(p,v){
+    return p + "" + v;
+}); // ->"Tom Huck Becky"
+
+var a = ["Tom","Huck","Becky"];
+a.reduce(function(p,v){
+    p[v]=v.length;
+    return p
+},{}); //->{Tom:3,Huck:4,Becky:6}
